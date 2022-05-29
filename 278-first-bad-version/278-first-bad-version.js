f95895385a445bf53fd0c235131001e24b199e79
Vincent Tran <vincent.tran.ece@gmail.com>
2022-05-28 19:53:50 -0500

    Time: 62 ms (84.97%), Space: 41.3 MB (96.72%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.js
+++ 278-first-bad-version/278-first-bad-version.js
@@ -18,10 +18,20 @@ var solution = function(isBadVersion) {
      * @return {integer} The first bad version
      */
     return function(n) {
-        for(let i = 0;  i <= n; i++) {
-            if(isBadVersion(i)) {
-                return i
+        let right = n;
+        let left = 0;
+        let pivot;
+        
+        while(left < right) {
+            pivot = Math.floor(left + (right - left)/2);
+            
+            if(isBadVersion(pivot)) {
+                right = pivot;
+            } else {
+                left = pivot + 1;
             }
-        }
+        };
+        
+        return left;
     };
 };
