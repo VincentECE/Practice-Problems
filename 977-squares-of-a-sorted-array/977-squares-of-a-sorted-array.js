d870659e8b2ce14dc05e4cc71744253d248f03f6
Vincent Tran <vincent.tran.ece@gmail.com>
2022-06-02 13:16:03 -0500

    Time: 255 ms (8.58%), Space: 50.1 MB (5.47%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -6,45 +6,20 @@
 //two pointers approach
 var sortedSquares = function(nums) {
     let results = [];
-    let left, right;
-    let startPoint;
-    
-    if(nums.length === 1) return [Math.pow(nums[0], 2)];
     
     for(let i = 0; i < nums.length; i++) {
-        if(nums[i] >= 0) {
-            startPoint = i;
-            break;
-        }
+        results.push(Math.pow(nums[i], 2));
     }
     
-    if(startPoint !== undefined) {
-        right = startPoint;
-        left = startPoint -1;
-    } else {
-        right = nums.length;
-        left = nums.length-1;
-    }
+    console.log(results.sort(compareNumbers));
     
-    while(left !== -1 || right !== nums.length) {
-        if(left !== -1 && right !== nums.length) {
-            if(Math.abs(nums[right]) > Math.abs(nums[left])) {
-                results.push(Math.pow(nums[left], 2));
-                left--;
-            } else {
-                results.push(Math.pow(nums[right], 2));
-                right++;
-            }
-        } else if(left === -1 && right !== nums.length) {
-            results.push(Math.pow(nums[right], 2));
-            right++;
-        } else if(right === nums.length && left !== -1) {
-            results.push(Math.pow(nums[left], 2));
-            left--;
-        }
-    }
+    
     
     return results;
     
     
 };
+
+function compareNumbers(a, b) {
+    return a - b;
+}
