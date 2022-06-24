f9d578fad5abd8242cbde1aad0bcbf6e83160e48
Vincent Tran <vincent.tran.ece@gmail.com>
2022-06-23 19:59:17 -0500

    Time: 113 ms (27.07%), Space: 44.4 MB (25.07%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -1,41 +1,38 @@
 /**
  * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
  * }
  */
 /**
- * @param {ListNode} l1
- * @param {ListNode} l2
+ * @param {ListNode} list1
+ * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function(l1, l2) {
-    let list = new ListNode()
-    let head = list
+var mergeTwoLists = function(list1, list2) {
+    let list = new ListNode();
+    let head = list;
     
-    while (l1 !== null && l2 !== null) {
-	
-		// Select the smallest value from either linked list,
-		// then increment that list forward.
-        if (l1.val < l2.val) {
-            list.next = new ListNode(l1.val)
-            l1 = l1.next
-        } else {
-            list.next = new ListNode(l2.val)
-            l2 = l2.next
-        }
+    while(list1 !== null && list2 !== null) {
+       if(list1.val < list2.val) {
+           list.next = new ListNode(list1.val);
+           list1 = list1.next;
+       } else {
+           list.next = new ListNode(list2.val);
+           list2 = list2.next;
+       }
         
-        list = list.next
+        list = list.next;
     }
     
-	// It's possible that one linked list is shorter than the other so we just
-	// add on the remainder of the last linked list. It's already sorted :)
-    if (l1 !== null)
-        list.next = l1
-    if (l2 !== null)
-        list.next = l2
+    if(list1 !== null) {
+        list.next = list1;
+    } 
     
-	// return .next because this first element in the linkedlist is empty
-    return head.next
+    if(list2 !== null) {
+        list.next = list2;
+    }
+    
+    return head.next;
 };
