c0dc9efe492337f514f3889b8fbd951a5fdc5996
Vincent Tran <vincent.tran.ece@gmail.com>
2022-06-27 16:30:42 -0500

    Time: 100 ms (32.53%), Space: 42.2 MB (63.06%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const openingAsKeys = {'(': ')', '{': '}', '[': ']'};
+    let stack = [];
+    let isValid = true;;
+    
+    for(let i = 0; i < s.length; i++) {
+        let currentChar = s[i];
+        
+        if(openingAsKeys[currentChar] !== undefined){
+            stack.push(currentChar);
+        } else {
+            let anticipatedCloser = openingAsKeys[stack[stack.length-1]];
+            
+            if(currentChar === anticipatedCloser) {
+                stack.pop();
+            } else {
+                isValid = false;
+            }
+        }
+    }
+    
+    if(stack.length > 0) {
+        isValid = false;
+    }
+    
+    
+    return isValid;
+};
