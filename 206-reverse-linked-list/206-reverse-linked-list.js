b24c50dd754541f8183051d3f49228be4ebc0898
Vincent Tran <vincent.tran.ece@gmail.com>
2022-07-11 20:49:09 -0400

    Time: 130 ms (6.77%), Space: 44.9 MB (6.46%) - LeetHub

---
+++ 206-reverse-linked-list/206-reverse-linked-list.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    
+    if(head === null) return head;
+  
+    let newHead;
+    
+    let innerFunc = function(currentNode, previous) {
+        
+        if(currentNode.next !== null) {
+            innerFunc(currentNode.next, currentNode);
+        } else {
+            newHead = currentNode;
+        }
+        
+        currentNode.next = previous;
+    }
+    
+    innerFunc(head, null);
+    
+    console.log(newHead);
+    
+    return newHead;
+}
