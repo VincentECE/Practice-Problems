8992326353bbcfbcaaeb8223fb2f863172ed1af0
Vince <vincent.tran.ece@gmail.com>
2022-07-24 20:50:16 -0500

    adds notes to LC 718 maximum length of repeared subarray

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.js
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.js
@@ -3,63 +3,65 @@
  * @param {number[]} nums2
  * @return {number}
  */
+
+// see notes for an explanation of the approach
 var findLength = function(nums1, nums2) {
-    
+
     let maxCounter = 0;
     let nums1Length = nums1.length;
     let nums1Pointer = 0;
-    
+
     let nums2Length = nums2.length;
     let nums2Pointer = nums2Length-1;
-    
+
     let shrinkage = 0;
-    
-    
+
+
     for(let i = 1; i <= nums1Length + nums2Length - 1; i++) {
         let currentNums1Pointer = nums1Pointer;
         let currentNums2Pointer = nums2Pointer;
         let counter = 0;
-        
+
         // console.log('iteration: ', i);
-        
+
        for(let j = 0; j < i - shrinkage; j++) {
            // console.log(nums1[currentNums1Pointer], nums2[currentNums2Pointer]);
 
            if(nums1[currentNums1Pointer] === nums2[currentNums2Pointer]) {
                counter++;
-               
+
                if(counter > maxCounter) {
                    maxCounter = counter;
                }
-               
+
            } else {
                counter = 0;
            }
-          
+
            currentNums1Pointer++;
            currentNums2Pointer++;
-           
+
            if(currentNums1Pointer > nums1Length-1) {
                shrinkage++;
                // break;
            }
        }
          // console.log('counter: ', counter);
-        
+
         nums2Pointer--;
-        
+
         if(nums2Pointer < 0) {
             nums2Pointer = 0;
             shrinkage++;
             nums1Pointer++;
         }
-        
+
     }
-  
+
     return maxCounter;
 };
 
-            
+
         // 1 2 3 2 1
                 // 3 2 1 4 7
 
--- 718-maximum-length-of-repeated-subarray/NOTES.md
+++ 718-maximum-length-of-repeated-subarray/NOTES.md
@@ -1,1 +1,21 @@
-​
+​I'm particularly proud of this one because it's my own original approach that performs faster than 98.7% of JS submissions.
+
+This approach uses an advanced two pointer technique. Visually, the pointers are used to compare the arrays like:
+
+              1, 2, 3, 4, 5
+  1, 2, 3, 4, 5
+
+              1, 2, 3, 4, 5
+     1, 2, 3, 4, 5
+
+              1, 2, 3, 4, 5
+        1, 2, 3, 4, 5
+
+
+... and so on
+
+
+
+The inner loop starts out running only once, then two times, then 3, etc.
+
+After the bottom array shifts far enough to the right, then the number of comparisons we need to make start to subtract by 1. That's what the shrinkage variable accounts for.
